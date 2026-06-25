df6ae0e75124cb3df73a65c9a2f828d13e70ebb3
TheBlue <ma.sh106mahsami@gmail.com>
2026-06-26 01:04:34 +0430

    make some changes based on pipeline func

--- cnn.py
+++ cnn.py
@@ -6,10 +6,10 @@ import numpy as np
 
 image_size = 32
 no_of_classes = 43
-image_shape = (32,32,3)
+image_shape = x_test.shape[1:]
 
 
-model = models.Sequential([layers.Conv2D(32 , (3,3) , activation='relu' , padding='same' , input_shape = (32,32,3)),
+model = models.Sequential([layers.Conv2D(32 , (3,3) , activation='relu' , padding='same' , input_shape = image_shape),
                            layers.MaxPool2D((2,2)),
                            layers.Conv2D(64 , (3,3) , activation='relu' , padding='same'),
                            layers.MaxPool2D((2,2)),
@@ -21,7 +21,7 @@ model = models.Sequential([layers.Conv2D(32 , (3,3) , activation='relu' , paddin
 
 opt = Adam(learning_rate= 0.001)
 
-model.compile(optimizer =opt , loss = 'categorical_crossentropy', metrics= ['accuracy'])
+model.compile(optimizer =opt , loss = 'sparse_categorical_crossentropy', metrics= ['accuracy'])
 # model.summary()
 
 #test
@@ -29,11 +29,11 @@ x_test = np.random.rand(10, 32,32,3)
 y_test = np.zeros((10,43))
 y_test[np.arange(10), np.random.randint(0,43,10)] = 1.0
 
-history = model.fit(x_test,y_test , epochs = 2 , batch_size = 2)
-print('tested successfuly')
+# history = model.fit(x_test,y_test , epochs = 2 , batch_size = 2)
+# print('tested successfuly')
 
 
 
-# train = model.fit(x = train_batches , validation_data=(val_batches) ,  epochs = 20 , verbose = 2)
+# train = model.fit(x = x_train , y_train , epochs = 20 , batch_size = 32, validation_data = (x_test , y_test))
 
 # model.save('CNNModel.h5',include_optimizer=True)
